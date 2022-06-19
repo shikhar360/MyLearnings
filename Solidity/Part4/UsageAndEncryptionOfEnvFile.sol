2120b3955838c998db5b15b9205f623e67978282
Shikhar Pratap Verma <sssonishikhar360@gmail.com>
2022-06-19 15:37:28 +0530

    Update UsageAndEncryptionOfEnvFile.sol

--- Solidity/Part4/UsageAndEncryptionOfEnvFile.sol
+++ Solidity/Part4/UsageAndEncryptionOfEnvFile.sol
@@ -1,3 +1,9 @@
+//--------------------------------------------------------------------------------------------------------------
+    IN THIS FILE WE WILL LEARN WHAT IS THE USAGE OF ENV FILE AND HOW WE CAN ENCRYPT THAT
+//--------------------------------------------------------------------------------------------------------------
+
+
+
 RPC_URL = `https://kovan.infura.io/v3/fdaugdffdy`;
 PRIVATE_KEY = `0x25249684294964hackmatkarnabhai2484848457`; // some tools needs '0x' before the private key
 PRIVATE_KAY_PASSWORD = password;
@@ -7,6 +13,7 @@ PRIVATE_KAY_PASSWORD = password;
 
 
 A breif Intro about Environment Variables
+
 An environment variable is a variable whose value is set outside the program, typically through 
 functionality built into the operating system or microservice. An environment variable is made up of a name/
 value pair, and any number may be created and available for reference at a point in time.
@@ -20,10 +27,14 @@ To store the SENSITIVE information that we dont want it to be public we use ``.e
 
 
 To use the env file first you have to install the respected nvm package
+
+//---------------------------------------------------------------------------------------
+
 `` npm install dotenv``
 or
 `` yarn add dotenv``
 
+//---------------------------------------------------------------------------------------
 
 
 
@@ -33,10 +44,15 @@ Then go the file where you want to use the data of .env file and write this (dep
 
 
 
-
-
 After you have done this now you will have the access of allthe DATA of the ``.env`` file.
+
+
+
+
 Now you have to use the DATA that is in the env file by this way.
+
+
+//--------------------------------------------------------------------------------------
 
 `` const wallet =  new.ethers.Wallet(process.env.PRIVATE_KEY , provider)  ``
 or
@@ -44,11 +60,12 @@ or
 or
 console.log(process.env.RPC_URL) 
 
+//----------------------------------------------------------------------------------------
 
 
 
-
-
+////////////////////////////////////////////////
+NOW WHAT IS THE PROBLEM NOW
 
 The problem withe this method is that you can accidently pushed your ``.env`` file into github and 
 thats not we want , Or what happen if someone hacked your computer , he could directly visit to your 
@@ -57,18 +74,19 @@ One of the other method that we can use to make our .env file more secure is by 
 
 
 
-
 Encryption of the sensitive data is going to be very useful when you will start working out with real funds
 
-
+///////////////////////////////////////////
 
 
 
 To ENCRYPT your .env file or your SENSITIVE DATA 
 
 
+
 Follow these steps:
 
+//------------------------------------------------------------------------------
 1-- Run this command in terminal
      `` npm install fs-extra ``            // if you already installed ethers using npm then its OK
 
@@ -76,116 +94,124 @@ Follow these steps:
 
 3-- Write a async function and a require statement in the ``encrypt.js`` like this
 
+//-----------------------------------------------------------------------------------
 
 
 
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------------------------------------------------------
 ```
 
-const {ethers} = require('ethers');       // ignore in case if you are not using ethers.js
-const fs = require("fs-extra")            // used to read and write File   
-require("dotenv").config()                //  The ``.env`` file to get access
+          const {ethers} = require('ethers');       // ignore in case if you are not using ethers.js
+          const fs = require("fs-extra")            // used to read and write File   
+          require("dotenv").config()                //  The ``.env`` file to get access
 
 
 
 
-async function main(){
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
-const encryptedJsonKey = await wallet.encrypt(
-  process.env.PRIVATE_KEY_PASSWORD,                               //this will take 2 arguments private key 
-  process.env.PRIVATE_KEY                                         // and the password
-)
+          async function main(){
+          const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+          const encryptedJsonKey = await wallet.encrypt(
+            process.env.PRIVATE_KEY_PASSWORD,                               //this will take 2 arguments private key 
+            process.env.PRIVATE_KEY                                         // and the password
+          )
 
 
 
-console.log(encryptedJsonKey);                            // we will be able to see the encrypted object 
-
-
-
-
-//now we will write the encrypted object into the file
+          console.log(encryptedJsonKey);                            // we will be able to see the encrypted object 
 
 
 
 
-
-fs.writeFileSync("./.encyptedKey.json" , encryptedJsonKey);     
-// We are saving (or pushing) the encrypted object into the file named `` .encryptedKey.json ``
-
-
-//               After this by running the command in the terminal 
-//                     `` node encrypt.js ``
-//                   we will get a file named 
-//                   `` .encryptedKey.json``
+          //now we will write the encrypted object into the file
 
 
 
 
 
-
-// Also DONT FORGET TO WRITE THE FILE NAME IN GITIGNORE FILE ie .encryptedKey.json
-
-
-
-}
+          fs.writeFileSync("./.encyptedKey.json" , encryptedJsonKey);     
+          // We are saving (or pushing) the encrypted object into the file named `` .encryptedKey.json ``
 
 
+          //               After this by running the command in the terminal 
+          //                     `` node encrypt.js ``
+          //                   we will get a file named 
+          //                   `` .encryptedKey.json``
 
 
-// After you have done this process you can DELETE the PRIVATE_KEY and PRIVATE_KEY_PASSWORD variable from `` .env `` file
 
 
-main().then(()=> process.exit(0)).catch((error)=>{           //Just some stuffs to catch the errors
-  console.error(error);                                      // you can ignore this if you want to  
-  process.exit(1);
-});
 
 
-```
+          // Also DONT FORGET TO WRITE THE FILE NAME IN GITIGNORE FILE ie .encryptedKey.json
+
+
+
+          }
+
+
+
+
+          // After you have done this process you can DELETE the PRIVATE_KEY and PRIVATE_KEY_PASSWORD variable from `` .env `` file
+
+
+          main().then(()=> process.exit(0)).catch((error)=>{           //Just some stuffs to catch the errors
+            console.error(error);                                      // you can ignore this if you want to  
+            process.exit(1);
+          });
+
+
+          ```
+//--------------------------------------------------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 Now that you have done all these process just go ahead to your `` deploy.js `` file or anywhere you want to use that private key (in my case m going to the deploy.js file)
+
 
 There you have to READ it and USE it (in my case i have encrypted it with a password so i have to use it with the PRIVATE_KEY_PASSWORD)
 
 
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------------------------------------------------------
 ```
 
-NOW go into your deploy.js file 
+          NOW go into your deploy.js file 
 
 
 
-Earlier we were using 
-``const wallet =  new.ethers.Wallet(process.env.PRIVATE_KEY , provider) ``
+          Earlier we were using 
+          ``const wallet =  new.ethers.Wallet(process.env.PRIVATE_KEY , provider) ``
 
 
 
-But after the Encryption we will use it like this
+          But after the Encryption we will use it like this
 
 
 
-const encryptedJson = fs.readFileSync("./.encryptedKey.json" , "utf8");   //exactly same if you also have `` .encryptedKey.json ``file name
+          const encryptedJson = fs.readFileSync("./.encryptedKey.json" , "utf8");   //exactly same if you also have `` .encryptedKey.json ``file name
 
 
 
-let wallet = new ethres.Wallet.fromEncryptedJsonSync(        // a ethers.js function 
-//It takes 2 arguments 
-encryptedJson , 
-process.env.PRIVATE_KEY_PASSWORD
+          let wallet = new ethres.Wallet.fromEncryptedJsonSync(        // a ethers.js function 
+          //It takes 2 arguments 
+          encryptedJson , 
+          process.env.PRIVATE_KEY_PASSWORD
 
-)
+          )
 
 
-wallet = await wallet.connect(provider);
+          wallet = await wallet.connect(provider);
 
 
 
 ```
+//--------------------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
