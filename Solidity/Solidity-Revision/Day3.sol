4da94f8c3e58ec8be74a407c9e9cfee290874f83
Shikhar Pratap Verma <sssonishikhar360@gmail.com>
2022-06-26 12:46:40 +0530

    Create Day3.sol

---
+++ Solidity/Solidity-Revision/Day3.sol
@@ -0,0 +1,12 @@
+/*
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Notes
+------------------------------------------------------
+
+After you deploy a contract to Ethereum, it’s immutable, which means that it can never be modified or updated again.
+This is one reason security is such a huge concern in Solidity. If there's a flaw in your contract code, there's no way for you to patch it later.
+
+So if we hardCode a value it will be impossible to changeit in future , thats why we need some functions that can change the key part (dynamic things) 
+of contract that could be change when needed in future.
+
+*/
