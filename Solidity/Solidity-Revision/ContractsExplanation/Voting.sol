ee1303c65a870427417ca6567951ba1a375d8263
Shikhar Pratap Verma <sssonishikhar360@gmail.com>
2022-07-13 13:49:19 +0530

    Update Voting.sol

--- Solidity/Solidity-Revision/ContractsExplanation/Voting.sol
+++ Solidity/Solidity-Revision/ContractsExplanation/Voting.sol
@@ -4,50 +4,54 @@ pragma solidity ^0.8.0;
 
 contract Voting{
 
-address public electionCommissioner;
+address internal electionCommissioner;
 
 struct Candidate{
   string name ;
+  string electionSymbol ;
   uint voteCount;
 }
 
 Candidate[] candidates;
 
-mapping(address => bool) public hasVoted;
+mapping(address => bool)  hasVoted;
 
+event VotedAddress(address);
 
 constructor(){
  electionCommissioner = msg.sender ;
 }
 
-function addCandidate(string memory _name ) public onlyElectionCommissioner {
-  candidates.push(Candidate(_name , 0));
+function addCandidate(string memory _name , string memory _symbolUrl ) external onlyElectionCommissioner {
+  candidates.push(Candidate(_name , _symbolUrl ,  0));
 }
 
 
-function vote(uint _index )public{
+function vote(uint _index )external{
   require( hasVoted[msg.sender] == false , "Already Voted");
   hasVoted[msg.sender] = true;
   candidates[_index].voteCount ++;
-  
+  emit VotedAddress(msg.sender);
 }
 
 function getCandidate() external view returns(Candidate[] memory){
   return candidates;
 }
 
-function totalVotes(uint _index)public view returns(uint){
+function totalVotes(uint _index)internal view returns(uint){
 return candidates[_index].voteCount ;
 }
 
-function disqualifyCandidate(uint _index) public onlyElectionCommissioner {
+
+
+function disqualifyCandidate(uint _index) external onlyElectionCommissioner {
   candidates[_index] = candidates[candidates.length - 1];
   candidates.pop();
 }
 
 
 modifier onlyElectionCommissioner(){
-  require(msg.sender == electionCommissioner , "You are Not Commissioner BC");
+  require(msg.sender == electionCommissioner , "You are Not Commissioner");
   _;
 }
 
