1e79479c96d73f544625dd5c7ab9453896ffcf1b
Shikhar Pratap Verma <sssonishikhar360@gmail.com>
2022-07-17 14:54:54 +0530

    Update Level-1.sol

--- Solidity/Solidity-Revision/CryptoZombies/Level-1.sol
+++ Solidity/Solidity-Revision/CryptoZombies/Level-1.sol
@@ -60,18 +60,32 @@ uint[] dynamicArray;    // a dynamic Array - has no fixed size, can keep growing
  
  
 While writing a function in solidity. 
+
 There are two ways in which you can pass an argument to a Solidity function:
+
+
 By value, which means that the Solidity compiler creates a new copy of the parameter's value and passes it to your function. This allows your
 function to modify the value without worrying that the value of the initial parameter gets changed.
 By reference, which means that your function is called with a... reference to the original variable. Thus, if your
 function changes the value of the variable it receives, the value of the original variable gets changed.
+
+
 --Functions are public by default--
+
 Make functions private always and only make it public when you really want it to be public
+
 private function names has a convention of starting with an underscore (_).
+
 When the function doesn't actually change state in Solidity we could declare it as a view function.
+
 we can also mark a function pure when it is not accessing any data from the contract i.e depends on the arguments for returning any value.
-keccak256 is a hash function . A hash function basically maps an input into a random 256-bit hexadecimal number . keccak256 expects a single parameter of type bytes
+
+
+keccak256 is a hash function .
+A hash function basically maps an input into a random 256-bit hexadecimal number . keccak256 expects a single parameter of type bytes
 this means we have to wrap every argument into (" ") . It is a built in function.
+
+
 A example of typecast , means certain type of variable is not compatible with another type of variable which belongs to the same category .
 uint8 a = 5;
 uint b = 6;
@@ -79,8 +93,12 @@ uint b = 6;
 uint8 c = a * b;
 // we have to typecast b as a uint8 to make it work:
 uint8 c = a * uint8(b);
+
+
+
 Events are a way for your contract to communicate that something happened on the blockchain to your app front-end,
 which can be 'listening' for certain events and take action when they happen.
+
 This has 2 things 1st the event keyword that is present in the contract after the states declared,
 and the other thing is (emit) that is in the function which is waiting for something to happen so that it could emmit the message 
 */
