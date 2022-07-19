454f706ff1f68b7fab9c2b9a39a1eb22833beedb
Shikhar Pratap Verma <sssonishikhar360@gmail.com>
2022-07-19 20:06:04 +0530

    Create JobBoard.sol

---
+++ Solidity/Solidity-Revision/ContractsExplanation/JobBoard.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^ 0.8.0;
+
+contract JobBoard {
+
+    uint256 public jobID = 0;
+
+    struct Job {
+        uint id ;
+        string title;
+        string companyName;
+        string description;
+        string employmentType;
+        string location;
+        uint salary;
+        string contactEmail;
+    }
+
+    Job[] public jobsArray;
+    // mapping (uint => Job) public jobMap;
+    
+    function addJob(
+        
+        string memory _title,
+        string memory _companyName,
+        string memory _description,
+        string memory _employmentType,
+        string memory _location,
+        uint _salary,
+        string memory _contactEmail
+    )
+    public {
+        jobsArray.push(
+            Job( jobID , 
+             _title,
+            _companyName, 
+            _description, 
+            _employmentType, 
+            _location, 
+            _salary, 
+            _contactEmail)
+        );
+        jobID++;
+    }
+
+
+
+    function getJobs() public view returns(Job[] memory) {
+        return jobsArray;
+    }
+
+    function getAllJobsLength() public view returns(uint) {
+        return jobsArray.length;
+    }
+}
