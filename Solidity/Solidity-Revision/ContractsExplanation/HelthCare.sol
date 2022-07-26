3a260282756c2be874cb3045e4bab454392a19a8
Shikhar Pratap Verma <sssonishikhar360@gmail.com>
2022-07-26 16:40:39 +0530

    Update HelthCare.sol

--- Solidity/Solidity-Revision/ContractsExplanation/HelthCare.sol
+++ Solidity/Solidity-Revision/ContractsExplanation/HelthCare.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: Unlicense
+
 pragma solidity ^0.8.0;
 
-contract Healthcare {
-
-    address internal doctor;
-    uint internal patient_ID = 0;
-   
+contract Health {
+    address public doctor;
+    uint internal patientID = 0;
 
     struct Patient {
         uint id;
@@ -15,75 +14,89 @@ contract Healthcare {
         string sex;
         PatientDocument[] docs;
     }
-    
-    struct  PatientDocument{
+
+    struct PatientDocument {
         string docName;
-        string hash ;
+        string hash;
     }
-    
-   
+
     mapping(address => Patient) internal patientToAdd;
     mapping(address => uint) internal patientMap;
     mapping(address => bool) internal hasReg;
 
     Patient[] internal patientArray;
 
-    constructor () {
+    constructor() {
         doctor = msg.sender;
     }
 
-
-    function addPatient(  string memory _name, uint _age,string memory _sex  )  external  {
-        require(hasReg[msg.sender] == false , "Already Registered");
+    function addPatient(
+        string memory _name,
+        uint _age,
+        string memory _sex
+    ) external {
+        require(hasReg[msg.sender] == false, "Already Registered");
         patientToAdd[msg.sender].addr = msg.sender;
         patientToAdd[msg.sender].name = _name;
         patientToAdd[msg.sender].age = _age;
         patientToAdd[msg.sender].sex = _sex;
+
         patientArray.push(patientToAdd[msg.sender]);
-        patientMap[msg.sender] = patient_ID;
+
+        patientMap[msg.sender] = patientID;
+
         hasReg[msg.sender] = true;
-        patient_ID++;
+        patientID++;
     }
-    
-    function deletePatient(uint _index) external{  //we have to pass the id of the patient in this 
-        require(msg.sender == doctor , "Only Doctors can delete Patients");
+
+    function deletePatient(uint _index) external {
+        //we have to pass the id of the patient in this
+        require(msg.sender == doctor, "Only Doctors can delete Patients");
         delete patientArray[_index];
     }
 
-    
-   function updatePatient(string memory _name, uint _age,string memory _sex) external {
-    patientArray[patientMap[msg.sender]].name = _name;
-    patientArray[patientMap[msg.sender]].age = _age;
-    patientArray[patientMap[msg.sender]].sex = _sex;
+    function updatePatient(
+        string memory _name,
+        uint _age,
+        string memory _sex
+    ) external {
+        patientArray[patientMap[msg.sender]].name = _name;
+        patientArray[patientMap[msg.sender]].age = _age;
+        patientArray[patientMap[msg.sender]].sex = _sex;
+    }
 
-   }
+    function addToDocsArr(string memory _docName, string memory _docHash)
+        external
+    {
+        require(hasReg[msg.sender] == true, "Register First ");
 
-    function  addToDocsArr(string memory _docName , string memory _docHash) external {
-        require(msg.sender == patientArray[patientMap[msg.sender]].addr , "Register First Or Cant Add other's File" );
-        patientArray[patientMap[msg.sender]].docs.push(PatientDocument(_docName , _docHash));
-      
-    }   
+        patientArray[patientMap[msg.sender]].docs.push(
+            PatientDocument(_docName, _docHash)
+        );
+    }
 
     function deleteDocs(uint _index) external {
-        require(msg.sender == patientArray[patientMap[msg.sender]].addr , "You can,t DELETE other's file" );
-        patientArray[patientMap[msg.sender]].docs[_index] = patientArray[patientMap[msg.sender]].docs[patientArray[patientMap[msg.sender]].docs.length -1];
+        require(
+            msg.sender == patientArray[patientMap[msg.sender]].addr,
+            "You can,t DELETE other's file"
+        );
+        PatientDocument[] memory arr = patientArray[patientMap[msg.sender]]
+            .docs;
+        arr[_index] = arr[arr.length - 1];
         patientArray[patientMap[msg.sender]].docs.pop();
-    }    
-   
-    function getPatientsInfo() public view returns(Patient[] memory) {
+    }
+
+    function getPatientsInfo() external view returns (Patient[] memory) {
         return patientArray;
     }
 
-    function getDocsInfo() public view returns(PatientDocument[] memory) {
-     require(msg.sender == patientArray[patientMap[msg.sender]].addr , "Register First" );  
+    function getDocsInfo() external view returns (PatientDocument[] memory) {
+        require(hasReg[msg.sender] == true, "Register First");
         return patientArray[patientMap[msg.sender]].docs;
     }
 
-    function getId() public view returns(uint) {
-     require(msg.sender == patientArray[patientMap[msg.sender]].addr , "Register First" );  
+    function getId() external view returns (uint) {
+        require(hasReg[msg.sender] == true, "Register First");
         return patientArray[patientMap[msg.sender]].id;
     }
-
-
-    
 }
