6e6f818355547e9ee3a32f68fe75d998bb73d34f
shikhar360 <sssonishikhar360@gmail.com>
2022-08-29 13:45:11 +0530

    Learned About Webpack

--- Three.js/04-Webpack/src/script.js
+++ Three.js/04-Webpack/src/script.js
@@ -1,1 +1,33 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+console.log(THREE);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 4;
+camera.position.x = 2.5;
+camera.position.y = 1.5;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("canvas.webgl"),
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
