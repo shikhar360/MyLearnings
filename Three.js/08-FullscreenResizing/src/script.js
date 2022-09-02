5d7e648dac50b365bd9962908d3c9370f15cb5d8
shikhar360 <sssonishikhar360@gmail.com>
2022-09-02 12:29:47 +0530

    Resizing completed

--- Three.js/08-FullscreenResizing/src/script.js
+++ Three.js/08-FullscreenResizing/src/script.js
@@ -1,69 +1,120 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//-- We will do resizing by calling a event handler
+
+window.addEventListener("resize", () => {
+  //- Update the size
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //- Update the camera
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //- Update the renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // it will set the minimum of thetow val accoring to your device
+});
+
+//   //-- If we want to fullsccreen something (this wont work on safari)
+// window.addEventListener("dblclick", () => {
+//   if (!document.fullscreenElement) {
+//     canvas.requestFullscreen();
+//   } else {
+//     document.exitFullscreen();
+//   }
+// });
+
+window.addEventListener("dblclick", () => {
+  // If we want to fullsccreen something on SAFARI as well
+
+  const fullScreen =
+    document.fullscreenElement || document.webkitFullScreenElement;
+
+  if (!fullScreen) {
+    if (document.fullscreenElement) {
+      canvas.requestFullscreen();
+    } else if (document.webkitFullScreenElement) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else if (document.webkitFullScreenElement) {
+      document.weblitExitFullscreen();
+    }
+  }
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
