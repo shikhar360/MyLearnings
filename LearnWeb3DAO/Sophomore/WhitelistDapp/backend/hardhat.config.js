a56ed297018f461118480921d8cbf04d06acf73f
shikhar360 <sssonishikhar360@gmail.com>
2022-09-05 23:09:19 +0530

    Backend work completed

--- LearnWeb3DAO/Sophomore/WhitelistDapp/backend/hardhat.config.js
+++ LearnWeb3DAO/Sophomore/WhitelistDapp/backend/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const URL = process.env.ANKR_URL;
+const PRIVATE = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: URL,
+      accounts: [PRIVATE],
+    },
+  },
 };
