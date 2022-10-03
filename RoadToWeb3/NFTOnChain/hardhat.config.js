2521ad37e50d56247163217fc4d02fea72b6e44d
shikhar360 <sssonishikhar360@gmail.com>
2022-10-03 17:03:36 +0530

    Week3 completed

--- RoadToWeb3/NFTOnChain/hardhat.config.js
+++ RoadToWeb3/NFTOnChain/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
