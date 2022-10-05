4eb23e4ad468ca42db1e71951dc7facd2574d733
shikhar360 <sssonishikhar360@gmail.com>
2022-10-05 12:33:44 +0530

    changes made

--- LearnWeb3DAO/junior/testinglocally/hardhat.config.js
+++ LearnWeb3DAO/junior/testinglocally/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const TESTNET_RPC = process.env.TESTNET_RPC;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: TESTNET_RPC,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_API_KEY,
+    },
+  },
 };
--- LearnWeb3DAO/junior/testinglocally/scripts/deploy.js
+++ LearnWeb3DAO/junior/testinglocally/scripts/deploy.js
@@ -1,31 +1,40 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so verifyContract here is a factory for instances of our Verify contract.
+  */
+  const verifyContract = await ethers.getContractFactory("Verify");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // deploy the contract
+  const deployedVerifyContract = await verifyContract.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await deployedVerifyContract.deployed();
 
-  await lock.deployed();
+  // print the address of the deployed contract
+  console.log("Verify Contract Address:", deployedVerifyContract.address);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(10000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedVerifyContract.address,
+    constructorArguments: [],
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
