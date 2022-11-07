245e11bba724222d6ef8f52ac5ecc8a739a1bbc8
shikhar360 <sssonishikhar360@gmail.com>
2022-11-07 15:28:34 +0530

    sol checker completed

--- Solana/buildspace/intro-frontend/pages/index.tsx
+++ Solana/buildspace/intro-frontend/pages/index.tsx
@@ -4,14 +4,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+    const key = new web3.PublicKey(address)
+    setAddress(key.toBase58())
+
+    const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
+    connection.getBalance(key).then( balance =>{
+
+      setBalance(balance/web3.LAMPORTS_PER_SOL)
+    })
   }
 
   return (
